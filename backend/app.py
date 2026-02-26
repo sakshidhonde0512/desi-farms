0d25caf92efc77a12c76f01e9bb20569e777d24f
Sakshi Dhonde <sakshidhonde0512@gmail.com>
2026-02-26 15:05:48 +0530

    fixed requirements

--- backend/app.py
+++ backend/app.py
@@ -104,7 +104,7 @@ def create_app():
 # ==========================
 # RUN APP
 # ==========================
-app = create_app()
-
 if __name__ == "__main__":
-    app.run()
+    app = create_app()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
